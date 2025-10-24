c7a23ab8c550d7c48586f170216e12996b7fc8f0
imnbwd <NoahInChrist@outlook.com>
2025-10-25 07:35:27 +0800

    update

--- app/prop/page.tsx
+++ app/prop/page.tsx
@@ -3,6 +3,7 @@ import SampleComponent from "@/components/SampleComponent";
 import { useState } from "react";
 export default function Page() {
   const [data, setData] = useState("");
+  const [showPanel, setShowPanel] = useState(false);
   return (
     <div className="container mx-auto py-4 flex flex-col min-h-screen">
       <div className="text-lg">Page</div>
@@ -10,16 +11,22 @@ export default function Page() {
         message1="Hello"
         message2="World"
         message3="sss"
-        message4="OKO"
+        message4={data}
       />
-      <div>aaa</div>
       <button
-        className="p-2 bg-amber-700 rounded-md "
+        className="p-2 bg-amber-700 rounded-md self-start"
         onClick={() => setData("content from button")}
       >
         click me
       </button>
       <div>{data}</div>
+      <button
+        className="p-2 bg-amber-700 rounded-md self-start"
+        onClick={() => setShowPanel(!showPanel)}
+      >
+        show or hide panel
+      </button>
+      {showPanel && <div className="mt-2 bg-sky-600 p-4 rounded-md w-48 h-24" />}
     </div>
   );
 }
